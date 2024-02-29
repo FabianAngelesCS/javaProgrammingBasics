1e51fcb2383fe8889b9523f157bc9152272581a1
FabianAngelesCS <cruzfabian292@gmail.com>
2024-02-29 14:03:48 -0600

    primeras dos lecciones

---
+++ out/src/WhatISProgramming.java
@@ -0,0 +1,13 @@
+public class WhatISProgramming {
+
+    /*---¿Que es programación?---
+
+
+    Programación es el proceso de diseñar, escribir, probar
+    y mantener el código de computadora para crear software,
+    aplicaciones y sistemas informáticos. En esencia, es el
+    acto de dar instrucciones a una computadora para que realice
+    tareas específicas.
+    """
+    */
+}
