d6e5ce071961dafec243dfa27b8a331419e14afb
FabianAngelesCS <cruzfabian292@gmail.com>
2024-02-29 14:17:42 -0600

    primer programa

---
+++ out/src/PremierProgram.java
@@ -0,0 +1,9 @@
+public final class PremierProgram {
+    public static void main(String[] args) {
+
+        String saludo = "Hello World, is this my class premier!!";
+
+        System.out.println(saludo);
+    }
+}
+
