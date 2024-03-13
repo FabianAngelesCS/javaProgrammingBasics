65c57d3a3f39ae67d60e507ebc8bcc67ceb59337
Fabian <cruzfabian292@mail.com>
2024-03-12 21:53:57 -0600

    POO

---
+++ out/src/WhatIsPOO.java
@@ -0,0 +1,47 @@
+public class WhatIsPOO {
+
+/*
+
+""" Programación orientada a objetos """
+
+La programación orientada a objetos (POO) es un paradigma de programación que se basa en el concepto de
+"objetos" y "clases". En POO, los programas se estructuran en torno a objetos que representan entidades
+ del mundo real. Cada objeto tiene características (atributos o propiedades) y comportamientos (métodos
+ o funciones) asociados. Las clases son plantillas o moldes para crear objetos; definen la estructura y
+ el comportamiento común a un conjunto de objetos relacionados.
+
+Los principales conceptos de la programación orientada a objetos incluyen:
+
+Clases y objetos:
+Una clase es una plantilla que define la estructura y el comportamiento de los objetos. Un objeto es una
+instancia de una clase. Por ejemplo, una clase "Coche" puede tener atributos como "color" y "velocidad",
+y métodos como "acelerar" y "frenar". Un objeto particular de la clase Coche podría ser un coche rojo que
+viaja a 60 km/h.
+
+Encapsulación:
+Es el concepto de ocultar los detalles de implementación de un objeto y exponer solo una interfaz que otros
+objetos pueden utilizar para interactuar con él. Esto se logra definiendo atributos como privados y
+proporcionando métodos públicos para acceder y modificar esos atributos.
+
+Herencia:
+Permite que una clase (subclase) herede atributos y métodos de otra clase (superclase). Esto promueve la
+reutilización del código y la creación de una jerarquía de clases. Por ejemplo, una clase "Vehículo" podría
+ser una superclase de las clases "Coche" y "Camión".
+
+Polimorfismo:
+Es la capacidad de un objeto de tomar muchas formas. Permite que un objeto se comporte de manera diferente según
+el contexto. Por ejemplo, una función que acepta un parámetro de tipo "Vehículo" puede operar con un objeto de
+tipo "Coche" o "Camión", ya que ambos son subtipos de "Vehículo".
+
+Abstracción:
+Es el proceso de identificar las características esenciales de un objeto y ignorar los detalles norelevantes.
+Permite modelar objetos del mundo real de manera más simple y comprensible.
+
+La programación orientada a objetos se utiliza ampliamente en el desarrollo de software debido a su capacidad para
+modelar problemas complejos de manera más organizada y modular. Ofrece ventajas como la reutilización del código,
+el aumento de la mantenibilidad y la extensibilidad del software, y una representación más fiel de las relaciones
+entre los componentes del sistema.
+
+
+ */
+}
