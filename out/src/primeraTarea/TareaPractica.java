c416f81104f5344cd69ea812b0605717e90ec034
FabianAngelesCS <cruzfabian292@gmail.com>
2024-03-21 17:40:58 -0600

    primera tarea practica

---
+++ out/src/primeraTarea/TareaPractica.java
@@ -0,0 +1,24 @@
+package primeraTarea;
+
+public class TareaPractica {
+    int age = 29;
+    double altura = 1.70;
+    String name = "Fabian Angeles Cruz Silva";
+    double gravedad = 9.816;
+
+    public static void main(String[] args) {
+        TareaPractica futbolista = new TareaPractica();
+
+        futbolista.name = "Lio Messi";
+        futbolista.age = 36;
+        futbolista.altura = 1.70;
+
+        String generoMusica = "Dance";
+        String mensaje = " me gusta el genero de musica, ";
+
+
+        System.out.println("Hola me llamo " + futbolista.name + mensaje + generoMusica);
+
+
+    }
+}
