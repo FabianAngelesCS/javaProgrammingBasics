03630d3fd59632b8101f01114832662e498025e5
FabianAngelesCS <cruzfabian292@gmail.com>
2024-04-05 17:10:31 -0600

    tarea 8 terminada

---
+++ out/src/sextaTareaCondicionales/tareaNumeroParImpar.java
@@ -0,0 +1,19 @@
+package sextaTareaCondicionales;
+
+import java.util.Scanner;
+
+public class tareaNumeroParImpar {
+
+    static void numeroParImpar(){
+        System.out.println("Ingresa un numero entero y te dire si es par o impar: ");
+        Scanner respuesta = new Scanner(System.in);
+        int numero = respuesta.nextInt();
+
+        String mensaje = numero % 2 == 0 ? "El número es par" : "El número es impar";
+        System.out.println(mensaje);
+    }
+
+    public static void main(String[] args) {
+        numeroParImpar();
+    }
+}
