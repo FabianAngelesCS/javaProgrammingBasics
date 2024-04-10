1773f999e1cc544249bba37de5877069c8d7bb7f
Fabian <cruzfabian292@mail.com>
2024-04-09 22:27:58 -0600

    tarea 11 terminada loop while

---
+++ out/src/tarea8WhileLoop/Ejercicio3Reversa.java
@@ -0,0 +1,26 @@
+package tarea8WhileLoop;
+
+import java.util.Scanner;
+
+public class Ejercicio3Reversa {
+
+    static void leerReversa(){
+        System.out.println("Ingresa una palabra y yo la devolvere escrita al reversa");
+        Scanner respuesta = new Scanner(System.in);
+        String palabra = respuesta.nextLine();
+
+        int longitud = palabra.length();
+        String textoInvertido = "";
+        int indice = longitud - 1;
+
+        while (indice >= 0){
+            textoInvertido += palabra.charAt(indice);
+            indice--;
+        }
+        System.out.println("La palabra escrita al reves queda asi: " + textoInvertido);
+    }
+
+    public static void main(String[] args) {
+        leerReversa();
+    }
+}
