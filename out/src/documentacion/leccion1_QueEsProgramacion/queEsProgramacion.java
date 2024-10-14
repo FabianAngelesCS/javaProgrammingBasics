48aa7712bc09c375ea99e921202dd0dd9a6da43c
FabianAngelesCS <cruzfabian292@gmail.com>
2024-10-13 22:36:42 -0600

    primer commit para rehacer los apuntes del curso

---
+++ out/src/documentacion/leccion1_QueEsProgramacion/queEsProgramacion.java
@@ -0,0 +1,25 @@
+package documentacion.leccion1_QueEsProgramacion;
+/*
+¿QUE ES PROGRAMACIÓN?
+
+La programación es el proceso de crear instrucciones detalladas que le indican a una computadora  que hacer para
+resolver un problema o realizar una tarea específica.
+Estas instrucciones se escriben en un lenguaje de programación que la computadora
+puede interpretar o compilar para ejecutar dichas acciones.
+Aunque no solo se trata de dar indicaciones, pues también se deben:
+
+- **Abstraer problemas** del mundo real en pasos lógicos para que una maquina pueda reseguirlos.
+- **Diseñar soluciones** eficientes y claras, que incluyan manejos de datos, control de flujo(decisiones, bucles) y la
+reutilización de código.
+- **Probar** las soluciones y corregir errores(debugging) para asegurar que fusionen como se espera.
+
+Una definición más completa podría ser algo como: "La programación es el acto de crear instrucciones precisas y lógicas
+que resuelven problemas"
+ */
+public class queEsProgramacion {
+    // Primer indicacion a una computadora...
+    // Debe imprimir un texto por consola
+    public static void main(String[] args) {
+        System.out.println("Hello wordl...loock at me, this is my firts programn in java");
+    }
+}
