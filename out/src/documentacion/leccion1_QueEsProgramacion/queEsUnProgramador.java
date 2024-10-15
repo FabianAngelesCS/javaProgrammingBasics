16b92a8c623dc16741adbb3d999a604aca2f89fc
FabianAngelesCS <cruzfabian292@gmail.com>
2024-10-14 22:22:00 -0600

    dafinicion de programador

---
+++ out/src/documentacion/leccion1_QueEsProgramacion/queEsUnProgramador.java
@@ -0,0 +1,18 @@
+package documentacion.leccion1_QueEsProgramacion;
+/*
+¿QUÉ ES UN PROGRAMADOR?
+
+Es una persona encargada de crear soluciones computacionales mediante la escritura de código en uno o varios lenguajes
+de programación, con el objetivo de automatizar tareas, resolver problemas complejos, y desarrollar aplicaciones de
+software.
+
+ */
+public class queEsUnProgramador {
+
+    public static void main(String[] args) {
+        //Ejemplo de una instrucción dada a una computadora
+        String mensaje = "Hola, esto es un string(cadena de texto) para ser impreso por consola";
+        System.out.println("Imprimir mensaje en consola: " + mensaje);
+    }
+
+}
