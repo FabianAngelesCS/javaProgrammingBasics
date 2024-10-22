de4dbd8f6eaf5f2e0f513e2d6b53c030ac0ac08c
FabianCSilva <fabian.cruz@123sourcing.com>
2024-10-22 17:24:16 -0600

    primer programa en java

--- out/src/documentacion/leccion3_instalacionHerramientas/InstalacionIntellij.java
+++ out/src/documentacion/leccion3_instalacionHerramientas/InstalacionIntellij.java
@@ -1,4 +1,18 @@
 package documentacion.leccion3_instalacionHerramientas;
+/*
+Para realizar la instalacion de INTELLIJ IDEA + JDK
+podemos seguir los pasos del siguiente  link para Windows:
+https://www.youtube.com/watch?v=-QG4TFNKM2w
 
+para mac:
+https://www.youtube.com/watch?v=VSRsqbvIqpQ
+
+ */
 public class InstalacionIntellij {
+    public static void main(String[] args) {
+        String mensaje = "Estamos preparando el taller de trabajo";
+
+        System.out.println(mensaje);
+    }
+
 }
